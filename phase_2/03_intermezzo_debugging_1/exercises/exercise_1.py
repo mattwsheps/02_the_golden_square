b395e5881edf89674010d62019a07366bb0cea64
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2023-11-14 15:32:07 +0000

    Debugging 1 Complete

---
+++ phase_2/03_intermezzo_debugging_1/exercises/exercise_1.py
@@ -0,0 +1,8 @@
+def say_hello(name):
+    return f"hello {name}"
+
+print(say_hello("kay"))
+# Intended output:
+#
+# > say_hello("kay")
+# => "hello kay"
---
+++ phase_2/03_intermezzo_debugging_1/exercises/exercise_2.py
@@ -0,0 +1,47 @@
+def encode(text, key):
+    cipher = make_cipher(key)
+    print(cipher)
+    ciphertext_chars = []
+    for i in text:
+        ciphered_char = chr(65 + cipher.index(i))
+        ciphertext_chars.append(ciphered_char)
+
+    return "".join(ciphertext_chars)
+
+
+def decode(encrypted, key):
+    cipher = make_cipher(key)[::-1]
+
+    plaintext_chars = []
+    for i in encrypted:
+        ord_i = ord(i)
+        plain_char = cipher[64 - ord(i)]
+        plaintext_chars.append(plain_char)
+
+    return "".join(plaintext_chars)
+
+
+def make_cipher(key):
+    alphabet = [chr(i + 97) for i in range(0, 26)]
+    cipher_with_duplicates = list(key) + alphabet
+
+    cipher = []
+    for i in range(0, len(cipher_with_duplicates)):
+        if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
+            cipher.append(cipher_with_duplicates[i])
+
+    return cipher
+
+# When you run this file, these next lines will show you the expected
+# and actual outputs of the functions above.
+print(f"""
+Running: encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+Expected: EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL
+Actual: {encode('theswiftfoxjumpedoverthelazydog', 'secretkey')}
+""")
+
+print(f"""
+Running: decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+Expected: theswiftfoxjumpedoverthelazydog
+Actual: {decode('EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL', 'secretkey')}
+""")
